2579d570723fa8dd6af6fe7fd7e6b40323379a6c
Dave Staddon <dstaddon@users.noreply.github.com>
2022-10-28 10:11:14 +0100

    More corections

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -35,6 +35,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -48,7 +49,6 @@ module "blog_sg" {
 
   name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
-
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["86.163.114.162/32"]
