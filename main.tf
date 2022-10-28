14067e1b5132b4a3be09e6f81f2965a43b48011f
Dave Staddon <dstaddon@users.noreply.github.com>
2022-10-28 10:06:18 +0100

    Correctins

--- main.tf
+++ main.tf
@@ -45,9 +45,10 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.0"
-  name    = "blog"
 
+  name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
+
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["86.163.114.162/32"]
